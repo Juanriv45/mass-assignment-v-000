b29d67abc60be3770b3534ec2ffb129e1cf0bc17
Juan Rivera <juanriv45@gmail.com>
2020-06-21 03:09:20 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,20 @@
 class Person
+<<<<<<< HEAD
+=======
+  #your code here
+>>>>>>> 4eaad45ce4642fd4b5bf9cdcfbc5a5a17f7e1f08
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
+<<<<<<< HEAD
   def initialize(attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+=======
+  def initialize(list)
+    list.each do |key,value|
+      self.send(("#{key}="),value)
+    end
+>>>>>>> 4eaad45ce4642fd4b5bf9cdcfbc5a5a17f7e1f08
   end
 end
